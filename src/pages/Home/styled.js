bd9cb025bd76eed16ba10f275a2e3cf05cc9b279
Rodrigo Emanuel <drigosantos@gmail.com>
2020-05-19 16:46:24 -0300

    Configurado botão 'Voltar' com componente.

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100vw;
     height: 100vh;
+    margin-top: 100px;
     display: flex;
     align-itens: center;
     justify-content: center;
