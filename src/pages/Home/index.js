7c3ee6cb9bf049e5d3290eb90bddce1e3231f96d
Rodrigo Emanuel <drigosantos@gmail.com>
2020-05-19 15:58:30 -0300

    Salvando dados no LocalStorage do navegador ao clicar.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,12 +7,19 @@ function App(props) {
   const [ usuario, setUsuario ] = useState('');
   
   function handlePesquisa(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
+      const repositories = response.data;
+      const repositoriesName = [];
+      repositories.map((repository) => {
+        repositoriesName.push(repository.name);
+      });
+      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+    });
   }
 
   return (
     <S.Container>
-    <p>{ usuario }</p>
+    {/* <p>{ usuario }</p> */}
       <S.Input type="text" className="usuarioInput" value={ usuario } placeholder="Usuário..." onChange={e => setUsuario(e.target.value)} />
       <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
     </S.Container>
